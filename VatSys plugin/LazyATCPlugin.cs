3dca54aec8e53f1124d0751898969772607c5ff8
Hedgehog-Aviation <handybluehedgehog@gmail.com>
2025-10-19 16:40:17 +1100

    add actual code to the file

--- VatSys plugin/LazyATCPlugin.cs
+++ VatSys plugin/LazyATCPlugin.cs
@@ -1,1 +1,51 @@
+using System;
+using System.ComponentModel.Composition;
+using System.Windows.Forms;
+using vatsys;
+using vatsys.Plugin;
 
+namespace LazyATCPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class LazyATCPlugin : IPlugin
+    {
+        public string Name => "LazyATC";
+
+        private static CustomToolStripMenuItem LazyATCMenu;
+        private static LazyATCWindow LazyATCWindow;
+
+        public LazyATCPlugin()
+        {
+            LazyATCMenu = new CustomToolStripMenuItem(CustomToolStripMenuItemWindowType.Main, CustomToolStripMenuItemCategory.Tools, new ToolStripMenuItem("LazyATC"));
+            LazyATCMenu.Item.Click += LazyATCMenu_Click;
+            MMI.AddCustomMenuItem(LazyATCMenu);
+        }
+
+        private void LazyATCMenu_Click(object sender, EventArgs e)
+        {
+            MMI.InvokeOnGUI((MethodInvoker)delegate ()
+            {
+                if (LazyATCWindow == null || LazyATCWindow.IsDisposed)
+                {
+                    LazyATCWindow = new LazyATCWindow();
+                }
+                else if (LazyATCWindow.Visible) return;
+
+                LazyATCWindow.Show();
+            });
+        }
+
+        public void OnLoad() { }
+
+        public void OnUnload() { }
+        public void OnFDRUpdate(FDP2.FDR updated)
+        {
+            // Not used in this plugin
+        }
+
+        public void OnRadarTrackUpdate(RDP.RadarTrack updated)
+        {
+            // Not used in this plugin
+        }
+    }
+}
